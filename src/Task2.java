e0ddffe0c357b4a7ffb6a136f4cedb7c9dfce9be
Stoyan Varadinov <varadinovs@gmail.com>
2023-07-08 19:47:11 +0300

    Homework 2/Task 2

---
+++ src/Task2.java
@@ -0,0 +1,10 @@
+/*
+Задача 2: Да се изведат на екрана числат от -20 до 50.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = -20; i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
