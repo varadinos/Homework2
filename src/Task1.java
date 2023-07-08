0bee1dcbf268d1917af2d4b0d5d6125acfac6ea5
Stoyan Varadinov <varadinovs@gmail.com>
2023-07-08 19:38:27 +0300

    Homework 2/Task 1

---
+++ src/Task1.java
@@ -0,0 +1,17 @@
+/*
+Задача 1: Да се изведат на екрана числата от 1 до 100..
+ */
+public class Task1 {
+    public static void main(String[] args) {
+
+          for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+          }
+
+       // int i = 1;
+       // while (i <= 100){
+       //     System.out.println(i);
+       //     i++;
+        }
+    }
+}
